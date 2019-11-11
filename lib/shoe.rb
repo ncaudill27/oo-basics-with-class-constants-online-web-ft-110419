9077e8eb858ed998c13842301d826646aed07d0e
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-11 21:56:52 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,6 +6,7 @@ class Shoe
   
   def initialize(brand)
     @brand = brand
+    BRANDS.include?(brand) ? nil : BRANDS << brand
   end
 
   def cobble
@@ -13,8 +14,4 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-  def brand=(brand)
-    BRANDS << brand
-  end
-  
 end
